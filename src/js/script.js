81df080ac77f2ec10eaefd90d133674899f24b60
Evgen Chushko <jeka.ch16@gmail.com>
2025-05-16 22:22:23 +0300

    tryharding

--- src/js/script.js
+++ src/js/script.js
@@ -5,7 +5,7 @@ import JustValidate from "just-validate";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import "/src/sass/style.scss";
+import "/ceramic/src/sass/style.scss";
 
 const burger = document.querySelector(".burger"),
 	close = document.querySelector(".header__menu-close"),
